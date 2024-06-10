1aedc61d68d15ff28769e8d0f3c900cfe4d7950b
dineshmehra86 <dinesh.mehra86@gmail.com>
2024-06-10 16:25:29 +0530

    page is finalized

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
+// This is whatsApp opening code
 function openWhatsApp() {
   // Replace '1234567890' with the phone number you want to open WhatsApp with
   var phoneNumber = '9312720072';
-  window.open('https://wa.me/' + phoneNumber, '_blank');
+  var welcomeMessage = encodeURIComponent("Hello, Thanks for connecting I am Atul, How may I help you");
+  window.open('https://wa.me/' + phoneNumber + '?text=' + welcomeMessage, '_blank');
 }
+
+// This page scroll code on navigation link click
+function scrollToSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+}
+
+
+// This is backtotop code
+window.addEventListener('scroll', function() {
+  document.body.classList.toggle('scrolled', window.scrollY > 0);
+});
