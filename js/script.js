2959676f70deef17168dd509a3a9539371aa6213
dineshmehra86 <dinesh.mehra86@gmail.com>
2024-06-10 21:21:01 +0530

    box height issues fix1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 // This is whatsApp opening code
 function openWhatsApp() {
   // Replace '1234567890' with the phone number you want to open WhatsApp with
-  var phoneNumber = '9312720072';
+  var phoneNumber = '9599872835';
   var welcomeMessage = encodeURIComponent("Hello, Thanks for connecting I am Atul, How may I help you");
   window.open('https://wa.me/' + phoneNumber + '?text=' + welcomeMessage, '_blank');
 }
+
+
+
 
 // This page scroll code on navigation link click
 function scrollToSection(sectionId) {
@@ -19,3 +22,4 @@ function scrollToSection(sectionId) {
 window.addEventListener('scroll', function() {
   document.body.classList.toggle('scrolled', window.scrollY > 0);
 });
+
