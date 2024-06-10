1ed50066295d46c5de226acaa355bf0bf22a5066
dineshmehra86 <dinesh.mehra86@gmail.com>
2024-06-10 11:10:58 +0530

    apply now button event is added

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function openWhatsApp() {
+  // Replace '1234567890' with the phone number you want to open WhatsApp with
+  var phoneNumber = '9312720072';
+  window.open('https://wa.me/' + phoneNumber, '_blank');
+}
